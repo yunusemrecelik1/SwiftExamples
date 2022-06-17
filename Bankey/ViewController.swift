555371e186fada296d4d5b56429446b3fc99a600
Yunus Emre Çelik <yemrecelikofficial@gmail.com>
2022-06-17 21:40:11 +0300

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Yunus Emre Çelik on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
